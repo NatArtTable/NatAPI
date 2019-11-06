9452687f5f363cf99be7fa5049080a63c3fe70d0
daniel.rocha <daniel.rocha@corp.globo.com>
2019-11-06 17:28:01 -0300

    Trying to prepare all repositories before starting the API.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/universal/stage/bin/nat-server api -Dconfig.file=conf/prod.conf -main com.danielsan.natapi.Main
+web: target/universal/stage/bin/nat-server -Dconfig.file=conf/prod.conf -main com.danielsan.natapi.Main
--- src/main/scala/com/danielsan/natapi/Main.scala
+++ src/main/scala/com/danielsan/natapi/Main.scala
@@ -2,20 +2,7 @@ package com.danielsan.natapi
 
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    if (args.length > 1) {
-      println("You must supply exact one argument! (options: prepare | api)")
-      sys.exit(1)
-    } else if (args.length == 0) {
-      println("You must supply exact one argument! (options: prepare | api)")
-      sys.exit(1)
-    } else {
-      args(0) match {
-        case "api"     => NatServer.start()
-        case "prepare" => NatServer.prepare()
-        case _ =>
-          println("Invalid argument! (options: prepare | api)")
-          sys.exit(1)
-      }
-    }
+    NatServer.prepare()
+    NatServer.start()
   }
 }

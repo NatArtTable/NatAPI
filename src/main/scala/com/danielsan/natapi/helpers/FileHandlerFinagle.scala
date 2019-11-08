a36f3ab70d6e8959c33feaf5aca7a7e770c151d2
daniel.rocha <daniel.rocha@corp.globo.com>
2019-11-08 18:26:09 -0300

    Creating tmp folder for uploaded files if not exists.

--- src/main/scala/com/danielsan/natapi/helpers/FileHandlerFinagle.scala
+++ src/main/scala/com/danielsan/natapi/helpers/FileHandlerFinagle.scala
@@ -1,7 +1,7 @@
 package com.danielsan.natapi.helpers
 
 import java.io.FileOutputStream
-import java.nio.file.{Path, Paths}
+import java.nio.file.{FileSystemException, Path, Paths}
 import java.util.UUID
 
 import com.danielsan.natapi.controllers.Controller
@@ -13,7 +13,17 @@ import com.typesafe.config.{Config, ConfigFactory}
 trait FileHandlerFinagle {
   private implicit val conf: Config = ConfigFactory.load()
 
-  private val tmpFolder = conf.getString("file_handler.tmp_folder")
+  private val tmpFolderPath = conf.getString("file_handler.tmp_folder")
+
+  private val tmpFolder = Paths.get(tmpFolderPath).toFile
+  if (tmpFolder.exists()) {
+    if (!tmpFolder.isDirectory) {
+      throw new Exception("Unable to create file tmp folder. File already existed and is not a folder")
+    }
+  } else {
+    val status = tmpFolder.mkdirs()
+    if (!status) throw new Exception("Unable to create tmp folder")
+  }
 
   implicit class FinagleFileHandler(file: FileUpload) extends FileHandler {
     override def fileType: FileType = file.contentType match {
@@ -28,7 +38,7 @@ trait FileHandlerFinagle {
         case file: OnDiskFileUpload => file.content.toPath
         case data: InMemoryFileUpload =>
           val bytes = Buf.ByteArray.Owned.extract(data.content)
-          val tmpPath = Paths.get(tmpFolder, UUID.randomUUID().toString)
+          val tmpPath = Paths.get(tmpFolderPath, UUID.randomUUID().toString)
           val fos = new FileOutputStream(tmpPath.toFile)
           fos.write(bytes)
 
